2d3fae0c7f2606d38aa801371afb23d4c6918c79
Shahed7676 <shahedsk1234@gmail.com>
2023-05-07 21:49:09 +0530

    added

--- grade the students 2/Solution.java
+++ grade the students 2/Solution.java
@@ -6,6 +6,27 @@ public class Solution {
     public static void main(String[] args) {
 Scanner sc=new Scanner(System.in);
         char val=sc.next().charAt(0);
+
+
+/*
+        if(val=='A'){
+            System.out.println("Excellent!");
+        }
+        else if(val=='B'){
+            System.out.println("Well done!");
+        }
+        else if(val=='C'){
+            System.out.println("You passed!");
+        }
+        else if(val=='F'){
+            System.out.println("Better luck next time!");
+        }
+        else{
+            System.out.println("Invalid grade");
+        }
+        */
+
+
  switch(val){
             case 'A':
                 System.out.println("Excellent!");
