cc889aa8aba9aa296850993e7f45117098426541
Shahed7676 <shahedsk1234@gmail.com>
2023-05-07 11:05:40 +0530

    added

---
+++ travel together/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Solution{
+        public static void main(String[] args) {
+    
+            Scanner sc=new Scanner(System.in);
+            int nCars=sc.nextInt();
+            int mCars=sc.nextInt();
+    
+                System.out.println(nCars*5+mCars*7);
+    
+            
+        }
+    }
